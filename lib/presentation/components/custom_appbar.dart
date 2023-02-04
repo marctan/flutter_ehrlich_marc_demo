e927e756e2b61c3310abea69af08bbc843b89742
marc tan <marcqtan@gmail.com>
2023-02-05 00:09:29 +0800

    Add clickable profile url link in appbar

--- lib/presentation/components/custom_appbar.dart
+++ lib/presentation/components/custom_appbar.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ehrlich_weather/utils/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({
     super.key,
     required this.title,
     required this.showLogout,
+    this.isLoading = false,
+    this.showUrl = false,
+    this.url,
     this.onClick,
   });
 
   final String title;
+  final Uri? url;
   final bool showLogout;
   final VoidCallback? onClick;
+  final bool isLoading;
+  final bool showUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +37,27 @@ class CustomAppBar extends StatelessWidget {
               width: 15,
             ),
             Expanded(
-              child: Text(
-                title,
-                style: Theme.of(context).textTheme.bodyText1,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    title,
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                  if (showUrl)
+                    InkWell(
+                      onTap: () async => await launchUrl(url!),
+                      child: Text(
+                        'Github Profile',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            ?.copyWith(color: Colors.blue),
+                      ),
+                    ),
+                ],
               ),
             ),
             if (showLogout)
@@ -40,14 +66,24 @@ class CustomAppBar extends StatelessWidget {
                   minimumSize: const Size(100, 40),
                   backgroundColor: Theme.of(context).primaryColor,
                 ),
-                onPressed: onClick,
-                child: Text(
-                  Label.logout,
-                  style: Theme.of(context).textTheme.bodyText2?.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                onPressed: isLoading ? null : onClick,
+                child: isLoading
+                    ? const SizedBox(
+                        height: 23,
+                        width: 23,
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    : Text(
+                        Label.logout,
+                        style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
                       ),
-                ),
               )
           ],
         ),
