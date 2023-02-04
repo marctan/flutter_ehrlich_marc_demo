5d74af632c322ef780c2f21593612b3096339922
marc tan <marcqtan@gmail.com>
2023-02-04 21:13:59 +0800

    Use dotenv to avoid exposing auth0 client id

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_ehrlich_weather/welcome_screen.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
