920577ba6a9d2c2d49ae259308b5b83edabe0cbb
marc tan <marcqtan@gmail.com>
2023-02-05 13:26:24 +0800

    Improve weather info screen

--- lib/presentation/screens/weather_screen.dart
+++ lib/presentation/screens/weather_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ehrlich_weather/domain/cubit/weather_cubit.dart';
+import 'package:flutter_ehrlich_weather/utils/constants.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key, required this.city});
@@ -22,6 +23,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: const Text('Weather Info')),
@@ -42,8 +45,99 @@ class _WeatherScreenState extends State<WeatherScreen> {
               );
             } else if (state.status == Status.loaded) {
               final weather = state.weather;
-              return Center(
-                child: Text(weather?.name ?? ''),
+              final icon = weather?.weather?.first.icon;
+              return SingleChildScrollView(
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Container(
+                      color: Theme.of(context).primaryColor,
+                      height: deviceSize.height * 0.35,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 7.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 40),
+                            child: Material(
+                              borderRadius: BorderRadius.circular(10),
+                              elevation: 10,
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 30),
+                                margin:
+                                    const EdgeInsets.symmetric(vertical: 30),
+                                height: deviceSize.height * 0.60,
+                                width: deviceSize.width * 0.8,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        _buildInfo(weather?.name ?? '',
+                                            Label.weatherCity),
+                                        if (icon != null)
+                                          Container(
+                                            color: Colors.redAccent,
+                                            child: Image.network(
+                                              fit: BoxFit.cover,
+                                              height: 65,
+                                              'http://openweathermap.org/img/wn/$icon@2x.png',
+                                            ),
+                                          )
+                                      ],
+                                    ),
+                                    _buildInfo(
+                                      '${weather?.mainWeather?.temp?.toString()} °С',
+                                      Label.temp,
+                                    ),
+                                    _buildInfo(
+                                      '${weather?.wind?.speed} m/s',
+                                      Label.windSpeed,
+                                    ),
+                                    _buildInfo(
+                                      '${weather?.clouds['all']} %',
+                                      Label.clouds,
+                                    ),
+                                    _buildInfo(
+                                      weather?.mainWeather?.pressure
+                                              ?.toString() ??
+                                          '',
+                                      Label.hpa,
+                                    ),
+                                    _buildInfo(
+                                      weather?.weather?.first.description
+                                              ?.toUpperCase() ??
+                                          '',
+                                      Label.description,
+                                    ),
+                                    _buildInfo(
+                                      weather?.coord['lon']?.toString() ?? '',
+                                      Label.lon,
+                                    ),
+                                    _buildInfo(
+                                      weather?.coord['lat']?.toString() ?? '',
+                                      Label.lat,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               );
             }
             return const Center(
@@ -52,6 +146,22 @@ class _WeatherScreenState extends State<WeatherScreen> {
           },
         ),
       ),
+    );
+  }
+
+  Widget _buildInfo(String title, String subtitle) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          subtitle,
+          style: const TextStyle(fontSize: 19, fontWeight: FontWeight.w300),
+        ),
+      ],
     );
   }
 }
